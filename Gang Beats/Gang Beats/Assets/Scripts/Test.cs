409ae4a8c2f0bafe797c4f87666cbc9095d7f071
EamonN <z5187554@ad.unsw.edu.au>
2019-10-30 13:48:05 +1100

    updated

---
+++ Gang Beats/Gang Beats/Assets/Scripts/Test.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public int score = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(gameObject);
+        score = score + 1;
+        Debug.Log(score);
+    }
+}
