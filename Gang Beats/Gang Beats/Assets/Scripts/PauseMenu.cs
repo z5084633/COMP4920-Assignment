544fd58cbf8a319959defbd9193985eec992677b
EamonN <z5187554@ad.unsw.edu.au>
2019-11-23 21:51:05 +1100

    pause menu

---
+++ Gang Beats/Gang Beats/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseMenuUI;
+    [SerializeField] private bool isPaused;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = !isPaused;
+        }
+        if(isPaused)
+        {
+            ActiveMenu();
+        }
+        else
+        {
+            DeactiveMenu();
+        }
+    }
+
+    void ActiveMenu()
+    {
+        Time.timeScale = 0;
+        AudioListener.pause = true;
+        pauseMenuUI.SetActive(true);
+    }
+
+    public void DeactiveMenu()
+    {
+        Time.timeScale = 1;
+        AudioListener.pause = false;
+        pauseMenuUI.SetActive(false);
+        isPaused = false;
+    }
+}
